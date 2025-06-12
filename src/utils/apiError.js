927350fcb6e65326d22d94c90cf790609b7e7866
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2025-06-12 11:29:01 +0530

    api and err conifigrations

---
+++ src/utils/apiError.js
@@ -0,0 +1,22 @@
+class apiError extends Error{
+    constructor(
+        statusCode,
+        message="Somthing went wrong",
+        errors = [],
+        statck = ""
+    ){
+        super(message)
+        this.message=message
+        this.statusCode=statusCode
+        this.errors= errors
+        this.success=false  
+        this.data=null
+        if (statck) {
+            this.statck=statck
+        } else {
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+
+}
+export { apiError}

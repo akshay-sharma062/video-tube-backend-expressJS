d1245b21b0895684349d289b619ab0191e2abd7e
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2025-06-16 15:19:37 +0530

    done project configrations

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+import fs from 'fs'
+
+cloudinary.config({ 
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key:process.envCLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+})
+
+
+const uploadOnCloudinary = (localFilePath)=>{
+try{
+    if (!localFilePath) return null
+    //upload files on cloudinary
+    const response = await cloudinary.uploader.upload(
+    localFilePath, {
+       resource_type:'auto',
+    }
+)
+//file has been uploaded success
+console.log('file has been uploaded on cloudinary',response.url)
+return response
+}
+catch(error) {
+    fs.unlinkSync(localFilePath)//remove the locally temp saved files as the operation failed
+    return null
+};
+}
+
+export {uploadOnCloudinary}
